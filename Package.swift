d22b603e416e5daf9f66baeadba06989a707065f
Abdullah Sönmez <99615897+abdullahsonmez49@users.noreply.github.com>
2023-12-16 23:09:45 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,20 +1,27 @@
-// swift-tools-version:5.9
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
     name: "IubendaMobileSDK",
+    defaultLocalization: "en",
     platforms: [
-        .iOS(.v12) // or the appropriate platform
-    ],
+           .iOS(.v12), // or the appropriate platform
+           .tvOS(.v12) // or the appropriate platform
+       ],
     products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "IubendaMobileSDK",
             targets: ["IubendaMobileSDK"]),
     ],
     targets: [
-        .binaryTarget(
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
             name: "IubendaMobileSDK",
-            path: "https://libraries.iubenda.com/ios/IubendaMobileSDK/iubenda.xcframework-2.7.1.zip"
-        )
+            path: "sdk"
+        ),
     ]
 )
